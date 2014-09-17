56345399e00cbef3bb6608c896b48bfebb3ad0aa
Sean <student@epicodus.com>
2014-09-17 15:21:40 -0700

    add search

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,5 +1,7 @@
 class SearchesController < ApplicationController
   def index
     @questions = Question.basic_search(params[:search_query])
+    @answers = Answer.basic_search(params[:search_query])
+    @results = @questions + @answers
   end
 end
