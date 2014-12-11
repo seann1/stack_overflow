842ccd0d02be5ec08be88749e4f0bc07bd85d11e
epicodus <student@epicodus.com>
2014-12-10 16:48:59 -0800

    add flash notice to login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
       session[:user_id] = user.id
       redirect_to root_url, notice: "Logged in!"
     else
-      flash.now.alert = "Email or password is invalid"
+      flash.now[:notice] = "Login failed"
       render "new"
     end
   end
