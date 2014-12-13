19aa89c9caeb2356891b7a555a0c10b16c6391db
Sean <student@epicodus.com>
2014-12-12 18:08:42 -0800

    add missing.png and change default file path

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   has_many :answers, dependent: :destroy
   has_many :votes
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.jpg"
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
