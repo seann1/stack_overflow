f78cf94cd432986265776488c6a5fa3f40e35793
Sean <student@epicodus.com>
2014-12-12 20:10:56 -0800

    add paperclip config to development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,4 +35,11 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
   Paperclip.options[:command_path] = "/usr/local/bin/"
+
+  config.paperclip_defaults = {
+  :storage => :s3,
+  :s3_credentials => {
+    :bucket => 'sean-stack'
+  }
+}
 end
