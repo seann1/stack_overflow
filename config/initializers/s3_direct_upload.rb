6b0e16180cecd7cc2babd0054f0b4585a945c53a
Sean <seanniesen@gmail.com>
2014-12-14 20:50:35 -0800

    checking gitignore again

--- config/initializers/s3_direct_upload.rb
+++ config/initializers/s3_direct_upload.rb
@@ -1,7 +1,7 @@
 S3DirectUpload.config do |c|
-  c.access_key_id = ""       # your access key id
-  c.secret_access_key = ""   # your secret access key
+  c.access_key_id = "AKIAJGQPHIXQSLQ6NRVA"       # your access key id
+  c.secret_access_key = "Md/oKmh1K/7WsGWaLMhEZMFPKyT7xVFmaglIQDf7"   # your secret access key
   c.bucket = "sean-stack"              # your bucket name
-  c.region = nil             # region prefix of your bucket url. This is _required_ for the non-default AWS region, eg. "s3-eu-west-1"
+  c.region = "s3-us-west-2"             # region prefix of your bucket url. This is _required_ for the non-default AWS region, eg. "s3-eu-west-1"
   c.url = nil                # S3 API endpoint (optional), eg. "https://#{c.bucket}.s3.amazonaws.com/"
 end
