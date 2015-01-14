ed1b14bd8e4b3f716ee01ee3c35f6cb50f1f8ff5
Sean <seanniesen@gmail.com>
2015-01-14 08:08:43 +0000

    change top again

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,4 @@
-StackOverflow.Application.configure do
+StackOverflow::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded on
--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,4 +1,4 @@
-StackOverflow.Application.configure do
+StackOverflow::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # The test environment is used exclusively to run your application's
